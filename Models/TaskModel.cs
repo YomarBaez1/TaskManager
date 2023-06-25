90a240552c52c1aa6e54c47848de134b0214bad3
Yomar Baez <yomarbaez2003@gmail.com>
2023-06-24 20:21:00 -0700

    Se agregó un nuevo modelo con el nombre de TaskModel y se agrego un nuevo controlador llamado TaskController.

---
+++ Models/TaskModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class TaskModel
+{
+    public int ID { get; set; }
+    public string Descripcion { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public string Estado { get; set; }
+    public int Prioridad { get; set; }
+}
