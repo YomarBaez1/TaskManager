f297cda1542ebbcf496665dd1e9f24352c664d7d
Yomar Baez <yomarbaez2003@gmail.com>
2023-06-24 20:44:41 -0700

    Se modifico el controlador TaskController para implementar las funciones principales del crud. Se creo una nueva carpeta llamada DAL y se agrego una nueva clase que contendra la logica del proyecto TaskDAL.cs

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,17 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using TaskManager.DAL;
+
 
 namespace TaskManager.Controllers
 {
     public class TaskController : Controller
     {
-        // GET: Task
+        private TaskDAL taskDAL;
+
+        public TaskController()
+        {
+            TaskDAL taskBLL = new TaskDAL();
+        }
+
+        // Acción Index: mostrar la lista de tareas existentes
         public ActionResult Index()
         {
             return View();
+        }
+
+        // Acción Create: formulario para agregar una nueva tarea
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TaskModel task)
+        {
+            return View();
+        }
+
+        // Acción Edit: formulario para editar una tarea existente
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(task);
+        }
+
+        // Acción Delete: eliminar una tarea
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
